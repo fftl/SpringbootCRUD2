0328c8d3166295575a7d70260a44d7d32d706d5e
fftl <fftl.0710@gmail.com>
2020-12-30 16:19:13 +0900

    little update

--- src/main/java/fftl/SpringbootCRUD2/controller/WebController.java
+++ src/main/java/fftl/SpringbootCRUD2/controller/WebController.java
@@ -1,10 +1,17 @@
 package fftl.SpringbootCRUD2.controller;
 
+import fftl.SpringbootCRUD2.model.User;
+import fftl.SpringbootCRUD2.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class WebController {
+
+    private final UserService userService;
 
     @GetMapping("/")
     public String goIndex(){
@@ -28,6 +35,14 @@ public class WebController {
 
     @GetMapping("/join")
     public String goJoin(){
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String userJoin(User user){
+
+        userService.userJoin(user);
+
         return "join";
     }
 
