35587f6144173df6d2749afa40497b99f8041ebc
fftl <fftl.0710@gmail.com>
2021-02-07 23:54:07 +0900

    update: project

--- src/main/java/fftl/SpringbootCRUD2/SpringbootCrud2Application.java
+++ src/main/java/fftl/SpringbootCRUD2/SpringbootCrud2Application.java
@@ -1,7 +1,9 @@
 package fftl.SpringbootCRUD2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class SpringbootCrud2Application {
